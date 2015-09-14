970f7b5280c71352b6c6801ab233433e25644c63
bonora.matteo@gmail.com <260uno1997>
2015-09-14 13:59:24 +0200

    Removed useless references

--- DODODoS/DODODoS/Program.cs
+++ DODODoS/DODODoS/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DODODoS
 {
--- DODODoS/DODODoS/UDP.cs
+++ DODODoS/DODODoS/UDP.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Net;
+﻿using System.Threading;
 using System.Net.Sockets;
 using System.Collections.Generic;
 
