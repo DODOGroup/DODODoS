a6e1f9c62b9c34999b5ab721b5851eea617aee55
hook25 <massigi@live.it>
2015-09-16 12:46:14 +0200

    Starting to develop chat

---
+++ DODODoS/DODODoS/littleChat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text
+
+namespace DODODoS{
+    static class littleChat{
+        public static readonly string Signature = "--Dodo--";
+        public static void Send(string s){
+            var udp = new UdpClient();
+            var mess = UTF8Encoding.UTF8.GetBytes(Signature + udp + Signature);
+            udp.Send(mess, mess.Length, new IPEndPoint(IPAddress.Broadcast, 666));
+        }
+        public static string Recive()
+        {
+            var udp = new UdpClient();
+            var ipe = new IPEndPoint(IPAddress.Any,666);
+            var mess = UTF8Encoding.UTF8.GetString(udp.Receive(ref ipe));
+            if()
+        }
+    }
+}
